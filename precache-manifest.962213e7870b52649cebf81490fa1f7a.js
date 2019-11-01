b00960b78167a1197a83c43dde12869967675130
Andrii <farikpite@gmail.com>
2019-11-01 23:26:59 +0200

    Updates

---
+++ precache-manifest.962213e7870b52649cebf81490fa1f7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa32de91cf90bb89aac54e07b95d1737",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "0045e8325dd0865109af",
+    "url": "/react_todo-app/static/css/main.f3af5957.chunk.css"
+  },
+  {
+    "revision": "a7474f14ab3e6b076318",
+    "url": "/react_todo-app/static/js/2.35e62f3c.chunk.js"
+  },
+  {
+    "revision": "0045e8325dd0865109af",
+    "url": "/react_todo-app/static/js/main.8c649678.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
